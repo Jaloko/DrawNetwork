e882242b3df904d76ac225b9a96cd5f59b72e8ae
Adam Holland <kiwiadam2@gmail.com>
2021-04-05 17:13:32 +1200

    chore: update dependencies to versions recent enough to work with Node 14
    Gulp had breaking changes to how task dependencies were defined which required gulpfile updates. Older versions of Gulp were not compatible with current versions of Node.

--- gulpfile.js
+++ gulpfile.js
@@ -22,12 +22,7 @@ var config = {
 	],
 	css: [
 		'client/css/**/.css'
-	],
-
-	tasks: {
-		default: ['clean', 'canvasjs', 'functionsjs', 'chartjs'],
-		dev: ['clean', 'dev-canvas', 'dev-functions', 'chartjs']
-	}
+	]
 }
 
 gulp.task('canvasjs', function() {
@@ -52,26 +47,26 @@ gulp.task('clean', function(){
 	return del(['client/js/dn.min.js', 'client/js/functions.min.js']);
 });
 
-gulp.task('dev-canvas', ['clean'], function(){
+gulp.task('dev-canvas', gulp.series('clean', function(){
 	return gulp.src(config.js)
 	.pipe(sourcemaps.init())
 	.pipe(concat('dn.min.js'))
 	.pipe(sourcemaps.write('./maps/'))
 	.pipe(gulp.dest('client/js'));
-});
+}));
 
-gulp.task('dev-functions', ['clean'], function(){
+gulp.task('dev-functions', gulp.series('clean', function(){
 	return gulp.src('client/js/functions.js')
 	.pipe(sourcemaps.init())
 	.pipe(concat('functions.min.js'))
 	.pipe(sourcemaps.write('./maps/'))
 	.pipe(gulp.dest('client/js'));
-});
+}));
 
 gulp.task('chartjs', function() {
-	return addsrc('node_modules/chart.js/Chart.min.js')
+	return gulp.src('node_modules/chart.js/Chart.min.js')
 	.pipe(gulp.dest('client/js'));
 });
 
-gulp.task('default', config.tasks.default);
-gulp.task('dev', config.tasks.dev);
+gulp.task('default', gulp.series('clean', gulp.parallel('canvasjs', 'functionsjs', 'chartjs')));
+gulp.task('dev', gulp.series('clean', gulp.parallel('dev-canvas', 'dev-functions', 'chartjs')));
